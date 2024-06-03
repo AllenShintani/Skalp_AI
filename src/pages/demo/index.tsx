aa6b495a6c6feecb19f0d08af4dc47a10c39fd43
AllenShintani <shintaniallen@gmail.com>
2024-06-03 13:59:02 +0900

    サイドバーの追加とボタンスタイルの調整

--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -1,7 +1,14 @@
+import DemoSidebar from '@/demoComponents/DemoSidebar'
 import DemoSlideEditor from '@/demoComponents/DemoSlideEditor'
+import styles from './Demo.module.css'
 
 const Demo = () => {
-  return <DemoSlideEditor />
+  return (
+    <div className={styles.container}>
+      <DemoSidebar />
+      <DemoSlideEditor />
+    </div>
+  )
 }
 
 export default Demo
