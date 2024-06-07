66f7c7118ea5cf8a8f4ff9f4ccaf4ee4ebbb9849
k-arikawa0312 <s1F102402710@iniad.org>
2024-06-07 23:03:24 +0900

    カスタムフックuseDrag,useResizeの作成

--- frontend/src/components/DraggableTextBox.tsx
+++ frontend/src/components/DraggableTextBox.tsx
@@ -28,18 +28,6 @@ const DraggableTextBox: React.FC<Props> = ({ textbox }) => {
   })
   const { handleDragMouseDown } = useDrag({ textbox })
 
-  // const [position, setPosition] = useState({ x: textbox.x, y: textbox.y })
-  // const [size, setSize] = useState({
-  //   width: textbox.width,
-  //   height: textbox.height,
-  // })
-  // const [isResizing, setIsResizing] = useState(false)
-  // const [resizeDirection, setResizeDirection] = useState<Direction>('default')
-  // const [resizeStart, setResizeStart] = useState<{ x: number; y: number }>({
-  //   x: size.width,
-  //   y: size.height,
-  // })
-
   const style: React.CSSProperties = {
     transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
     position: 'absolute',
@@ -49,44 +37,6 @@ const DraggableTextBox: React.FC<Props> = ({ textbox }) => {
     outline: textbox.isSelected ? 'solid 1px blue' : 'none',
     userSelect: 'none', // Prevent text selection(入力の無効化はtiptapにメソッドが存在する為、注意が必要)
   }
-
-  // const handleResizeMouseDown = useCallback(
-  //   (e: React.MouseEvent, direction: Direction) => {
-  //     e.stopPropagation()
-  //     setResizeDirection(direction)
-  //     setIsResizing(true)
-  //     setResizeStart({ x: e.clientX, y: e.clientY })
-  //     textbox.isSelected = true
-  //   },
-  //   [textbox],
-  // )
-  // const handleResizeMouseMove = useCallback(
-  //   (e: MouseEvent) => {
-  //     if (!isResizing) return
-  //     const newOptions = handleResize(
-  //       e,
-  //       resizeStart,
-  //       size,
-  //       position,
-  //       resizeDirection,
-  //     )
-  //     if (newOptions.width < -10 || newOptions.height < -10) return
-
-  //     setPosition({ x: newOptions.x, y: newOptions.y })
-  //     setSize({
-  //       width: newOptions.width,
-  //       height: newOptions.height,
-  //     })
-  //     setResizeStart({ x: e.clientX, y: e.clientY })
-  //   },
-  //   [isResizing, resizeStart, size, position, resizeDirection],
-  // )
-  // const handleResizeMouseUp = useCallback(() => {
-  //   if (!isResizing) return
-  //   setIsResizing(false)
-  //   setResizeDirection('default')
-  //   textbox.isSelected = false
-  // }, [isResizing, textbox])
 
   return (
     <div
