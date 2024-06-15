081df3a8ab0a236a5980374e7ece2db70ae8658f
skmtrd <s1f102400314@iniad.org>
2024-06-16 06:04:58 +0900

    feature/#54/作成ボタンの配置

--- frontend/src/pages/workspace/index.page.tsx
+++ frontend/src/pages/workspace/index.page.tsx
@@ -1,8 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faFire } from '@fortawesome/free-solid-svg-icons'
+import { useRouter } from 'next/router'
 
 import styles from '../../styles/WorkSpace.module.css'
 const WorkSpace = () => {
+  const router = useRouter()
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -20,7 +23,20 @@ const WorkSpace = () => {
       </div>
       <div className={styles.workSpaceSelector} />
       <div className={styles.projectSelector} />
-      <div className={styles.main} />
+      <div className={styles.main}>
+        <div className={styles.buttonBar}>
+          {/* todo: /pages/select-ui を作成して、そこに移動させる */}
+          <button>
+            <FontAwesomeIcon
+              className={styles.icon}
+              icon={faFire}
+            />
+            AIでプレゼン作成
+          </button>
+          {/* todo: slideidと紐づけ */}
+          <button onClick={() => router.push('/slide/1')}>プレゼン作成</button>
+        </div>
+      </div>
     </div>
   )
 }
